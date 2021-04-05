94d1274532f8e7f1d18dd39d11cf3e7be433903c
ibrahem <ibrahem.ahmed.ibrahem500@gmail.com>
2021-04-05 06:44:57 +0200

    added ingredients to recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,18 +1,21 @@
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingrediant.model';
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
     new Recipe(
-      'A Test Recipe',
-      'This is simply a test',
-      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
+      'Tasty Schnitezl',
+      'A super-tasty Schniztel - just awesome! ',
+      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+      [new Ingredient('meat', 1), new Ingredient('French Fries', 20)]
     ),
     new Recipe(
-      'Another Test Recipe',
-      'This is simply a test',
-      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
+      'Big Burger',
+      'this is greet!!',
+      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+      [new Ingredient('Bread', 1), new Ingredient('Meat', 20)]
     ),
   ];
 
