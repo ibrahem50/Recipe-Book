1988170e7872765b01a212261b30b30d5988e319
ibrahem <ibrahem.ahmed.ibrahem500@gmail.com>
2021-04-15 07:44:33 +0200

    storing recipes in firebase

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Recipe } from '../recipes/recipe.model';
+import { RecipeService } from '../recipes/recipe.service';
+
+@Injectable({ providedIn: 'root' })
+export class DataStorageService {
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http
+      .put(
+        'https://recipe-book-bbb97-default-rtdb.firebaseio.com/recipes.json',
+        recipes
+      )
+      .subscribe((res) => {
+        console.log(res);
+      });
+  }
+}
